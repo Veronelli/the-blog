c199f1452fefe274f73a3296a4932cfb5e9f5f3f
Facundo Veronelli <35713732+Veronelli@users.noreply.github.com>
2026-08-21 02:30:54 -0300

    remove view of profile view and remove url

--- project/profiles/views.py
+++ project/profiles/views.py
@@ -1,25 +1,3 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
-from profiles.models import PublicProfile, SocialNetworkInstance
-
-
-def public_profile(request: HttpRequest, public_username: str) -> HttpResponse:
-    profile = get_object_or_404(PublicProfile, public_username=public_username)
-    instances = SocialNetworkInstance.objects.filter(
-        author=profile.user, archived=False
-    )
-    instance_urls = [
-        {"instance": instance, "url": instance.url}
-        for instance in instances
-        if instance.url is not None
-    ]
-    return render(
-        request,
-        "profiles/public_profile.html",
-        {
-            "profile": profile,
-            "instances": instances,
-            "instance_urls": instance_urls,
-        },
-    )
+# Create your views here.
